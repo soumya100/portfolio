bc1dbdbf28a27a899c1e4a3e11c432104a07d341
soumya100 <47693557+soumya100@users.noreply.github.com>
2023-11-11 20:16:46 +0530

    first commit

---
+++ src/container/LandingPage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Box} from "@mui/material"
+import LandingPage from '@/components/LandingPage'
+
+const LandingPageIndex = () => {
+  return (
+    <Box>
+        <LandingPage />
+    </Box>
+  )
+}
+
+export default LandingPageIndex
